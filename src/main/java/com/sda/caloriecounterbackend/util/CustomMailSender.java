37def071c4db711df9e4751ac4cb55a2e24c1132
marek <marek.sadzynski@gmail.com>
2020-08-30 14:19:16 +0200

    EmailSender

---
+++ src/main/java/com/sda/caloriecounterbackend/util/CustomMailSender.java
@@ -0,0 +1,33 @@
+package com.sda.caloriecounterbackend.util;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
+import org.springframework.stereotype.Component;
+
+import javax.mail.MessagingException;
+import javax.mail.internet.MimeMessage;
+
+@Component
+@Slf4j
+public class CustomMailSender {
+    private final JavaMailSender mailSender;
+
+    public CustomMailSender(JavaMailSender mailSender) {
+        this.mailSender = mailSender;
+    }
+
+    public void sendMail(String sendTo, String topic, String content) {
+        MimeMessage mimeMailMessage = mailSender.createMimeMessage();
+        MimeMessageHelper helper = new MimeMessageHelper(mimeMailMessage);
+        try {
+            helper.setTo(sendTo);
+            helper.setSubject(topic);
+            helper.setText(content);
+        } catch (MessagingException e) {
+            log.error("Could not send mail: {}", e.getMessage());
+        }
+        mailSender.send(mimeMailMessage);
+        log.info("Mail send successfully to {}", sendTo);
+    }
+}
