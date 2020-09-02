37792b9ff01e0a913e3eca5bc6e0e76149b997bc
adam.zmijewski <adam.zmijewski@bluesoft.com>
2020-09-02 19:22:49 +0200

    Cors to login Controller

--- src/main/java/com/sda/caloriecounterbackend/controller/LoginController.java
+++ src/main/java/com/sda/caloriecounterbackend/controller/LoginController.java
@@ -3,15 +3,13 @@ package com.sda.caloriecounterbackend.controller;
 import com.sda.caloriecounterbackend.dto.LoginDto;
 import com.sda.caloriecounterbackend.service.LoginService;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 
 @RestController
 @RequestMapping("api/v1")
+@CrossOrigin(origins = "*")
 public class LoginController {
 
     private final LoginService loginService;
